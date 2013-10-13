242c50e5ae959a3f15b8db87291e0b963b4a9f77
Demian Neidetcher <demian0311@gmail.com>
2013-10-13 07:03:50 -0500

    cleanup

--- grails-app/services/hystrix/circuit/breaker/TestService.groovy
+++ grails-app/services/hystrix/circuit/breaker/TestService.groovy
@@ -14,30 +14,3 @@ class TestService {
         return result
     }
 }
-
-//class CommandServiceMethod extends HystrixCommand{
-//    String someState
-//
-//    def CommandServiceMethod(String stringIn){
-//        super(HystrixCommandGroupKey.Factory.asKey("ExampleGroup"));
-//        println("command created")
-//        someState = stringIn
-//    }
-//
-//    @Override
-//    def String run(){
-//        println("command called")
-//        def multiplier = (Math.random() * 100).toInteger()
-//        println("multiplier: " + multiplier)
-//
-//        if(multiplier % 9 == 0){
-//            throw new RuntimeException("something bad happened")
-//        } else if(multiplier % 7 == 0){
-//            Thread.sleep(10 * 1000)
-//        } else {
-//            Thread.sleep(multiplier * 10)
-//        }
-//
-//        someState.reverse()
-//    }
-//}
